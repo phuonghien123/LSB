4e68b14c0a1b802f482ed89c36da84a4a2e49b68
phuonghien123 <phuonghien.nguyenthi345@yahoo.com>
2018-04-19 00:50:08 +0700

    first commit

---
+++ LSB-PhuongHien/BitMapLSB/BitMapLSB/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace BitMapLSB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tapControl = new System.Windows.Forms.TabControl();
+            this.giauTin = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tachTin = new System.Windows.Forms.TabPage();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.lab = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tapControl.SuspendLayout();
+            this.giauTin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tachTin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tapControl
+            // 
+            this.tapControl.Controls.Add(this.giauTin);
+            this.tapControl.Controls.Add(this.tachTin);
+            this.tapControl.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tapControl.Location = new System.Drawing.Point(0, 0);
+            this.tapControl.Name = "tapControl";
+            this.tapControl.SelectedIndex = 0;
+            this.tapControl.Size = new System.Drawing.Size(870, 486);
+            this.tapControl.TabIndex = 5;
+            // 
+            // giauTin
+            // 
+            this.giauTin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.giauTin.Controls.Add(this.pictureBox1);
+            this.giauTin.Controls.Add(this.label1);
+            this.giauTin.Controls.Add(this.groupBox2);
+            this.giauTin.Controls.Add(this.groupBox1);
+            this.giauTin.Location = new System.Drawing.Point(4, 25);
+            this.giauTin.Name = "giauTin";
+            this.giauTin.Padding = new System.Windows.Forms.Padding(3);
+            this.giauTin.Size = new System.Drawing.Size(862, 457);
+            this.giauTin.TabIndex = 0;
+            this.giauTin.Text = "Giấu thông điệp";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(33, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(408, 314);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(9, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(33, 362);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(795, 56);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chọn ảnh";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Fuchsia;
+            this.button2.Location = new System.Drawing.Point(647, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Chèn thông điệp";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(6, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 25);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Chọn ảnh";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(464, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(461, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(367, 314);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông điệp";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 22);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(361, 286);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // tachTin
+            // 
+            this.tachTin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.tachTin.Controls.Add(this.pictureBox2);
+            this.tachTin.Controls.Add(this.groupBox4);
+            this.tachTin.Controls.Add(this.groupBox3);
+            this.tachTin.Location = new System.Drawing.Point(4, 25);
+            this.tachTin.Name = "tachTin";
+            this.tachTin.Padding = new System.Windows.Forms.Padding(3);
+            this.tachTin.Size = new System.Drawing.Size(862, 457);
+            this.tachTin.TabIndex = 1;
+            this.tachTin.Text = "Tách thông điệp";
+            this.tachTin.Click += new System.EventHandler(this.tachTin_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(30, 25);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(408, 314);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox4.Controls.Add(this.lab);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Location = new System.Drawing.Point(470, 25);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(355, 314);
+            this.groupBox4.TabIndex = 8;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Thông điệp";
+            this.groupBox4.Enter += new System.EventHandler(this.groupBox4_Enter);
+            // 
+            // lab
+            // 
+            this.lab.Location = new System.Drawing.Point(0, 28);
+            this.lab.Multiline = true;
+            this.lab.Name = "lab";
+            this.lab.Size = new System.Drawing.Size(355, 286);
+            this.lab.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Location = new System.Drawing.Point(30, 360);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(795, 58);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Chọn ảnh";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button4.Location = new System.Drawing.Point(12, 17);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 25);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Chọn ảnh";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Fuchsia;
+            this.button3.Location = new System.Drawing.Point(699, 17);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 25);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Giải mã";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(114, 19);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(579, 23);
+            this.textBox3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 498);
+            this.Controls.Add(this.tapControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tapControl.ResumeLayout(false);
+            this.giauTin.ResumeLayout(false);
+            this.giauTin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tachTin.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tapControl;
+        private System.Windows.Forms.TabPage giauTin;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TabPage tachTin;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox lab;
+    }
+}
+
---
+++ LSB-PhuongHien/BitMapLSB/BitMapLSB/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitMapLSB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.ShowDialog();
+            op.Filter = "txt file|*.txt";
+            textBox1.Text = op.FileName;
+            pictureBox1.Image = Image.FromFile(op.FileName);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+
+                try
+            {   
+                
+                Bitmap pm = new Bitmap(textBox1.Text);
+                int pmx = pm.Width;
+                int pmy = pm.Height;
+                int pmSetx = 0, pmSety = 0;
+                string num = Convert.ToString((textBox2.Text.Length * 8),2);
+                if(num.Length < 16)
+                {
+                    for(int i = 16 - num.Length; i > 0; i--)
+                    {
+                        num = '0' + num;
+                    }
+                }
+                for (int j = 0; j < num.Length; j++)
+                {
+                    Color cl = pm.GetPixel(pmSetx, pmSety);
+                    string cl2 = Convert.ToString(cl.R, 2);
+                    cl2 = cl2.Remove(cl2.Length - 2, 1) + num[j];
+                    double x = 0;
+                    for (int z = cl2.Length - 1; z >= 0; z--)
+                    {
+                        if (cl2[z] == '1')
+                            x += Math.Pow(2.0, cl2.Length - 1 - z);
+                    }
+                    Color newcl = Color.FromArgb(Convert.ToByte(x), cl.G, cl.B);
+                    pm.SetPixel(pmSetx, pmSety, newcl);
+                    pmSety++;
+                    if (pmSety == pmy)
+                    {
+                        pmSetx++;
+                        pmSety = 0;
+                    }
+                }
+                String s2 = textBox2.Text;
+                byte[] s2Byte = Encoding.Unicode.GetBytes(s2);
+                String s = "";
+                for (int i = 0; i < s2Byte.Count(); i+=2)
+                {
+                    s = Convert.ToString(s2Byte[i], 2);
+                    if (s.Length < 8)
+                    {
+                        for (int k = 8 - s.Length; k > 0; k--)
+                        {
+                            s = '0' + s;
+                        }
+                    }
+                    for (int j = 0; j < s.Length; j++)
+                    {
+                        Color cl = pm.GetPixel(pmSetx, pmSety);
+                        string cl2 = Convert.ToString(cl.R, 2);
+                        cl2 = cl2.Remove(cl2.Length - 2, 1) + s[j];
+                        double x = 0;
+                        for(int z = cl2.Length -1; z >= 0; z--)
+                        {
+                            if(cl2[z] == '1')
+                                x += Math.Pow(2.0,cl2.Length - 1 - z);
+                        }
+                        Color newcl = Color.FromArgb(Convert.ToByte(x), cl.G, cl.B);
+                        pm.SetPixel(pmSetx, pmSety, newcl);
+                        pmSety++;
+                        if (pmSety == pmy)
+                        {
+                            pmSetx++;
+                            pmSety = 0;
+                        }
+                    }
+                }
+                SaveFileDialog saveFile = new SaveFileDialog();
+                 saveFile.Filter = "Image Files (*.png, *.jpg) | *.png; *.jpg";
+                 saveFile.InitialDirectory = @"C:\User\metech\Desktop";
+                if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                 textBox1.Text = saveFile.FileName.ToString();
+                pictureBox1.ImageLocation = textBox1.Text;
+                pm.Save(textBox1.Text);
+                
+            };
+               
+            }
+            catch (Exception){
+                MessageBox.Show("Mời bạn chọn ảnh!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                Bitmap pm = new Bitmap(textBox3.Text);
+                int pmx = pm.Width;
+                int pmy = pm.Height;
+                string num = "", kq = "";
+                for (int i = 0; i < 16; i++)
+                {
+                    Color cl = pm.GetPixel(0, i);
+                    string clstr = Convert.ToString(cl.R, 2);
+                    num += clstr.Substring(clstr.Length - 1, 1);
+                }
+
+                double x = 0;
+                for (int z = num.Length - 1; z >= 0; z--)
+                {
+                    if (num[z] == '1')
+                        x += Math.Pow(2.0, num.Length - 1 - z);
+                }
+                int count = 0;
+                bool stop = false;
+                for (int j = 0; j < pmx - 1; j++)
+                {
+                    if (stop == true) break;
+                    for (int i = 0; i < pmy - 1; i++)
+                    {
+                        Color cl = pm.GetPixel(j, i);
+                        string clstr = Convert.ToString(cl.R, 2);
+                        kq += clstr.Substring(clstr.Length - 1, 1);
+                        count++;
+                        if (count >= Convert.ToInt32(x) + 16)
+                        {
+                            stop = true;
+                            break;
+                        }
+                    }
+                }
+                string output = "";
+                for (int i = 16; i < kq.Length - 1; i = i + 8)
+                {
+                    string temp = kq.Substring(i, 8);
+                    double xtemp = 0;
+                    for (int z = temp.Length - 1; z >= 0; z--)
+                    {
+                        if (temp[z] == '1')
+                            xtemp += Math.Pow(2.0, temp.Length - 1 - z);
+                    }
+                    output += (char)xtemp;
+                }
+                lab.Text = output;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ảnh không có thông điệp");
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tachTin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+                op.ShowDialog();
+                op.Filter = "txt file|*.txt";
+                textBox3.Text = op.FileName;
+                pictureBox2.Image = Image.FromFile(op.FileName);
+
+        }
+
+        private void groupBox4_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
